2ac1fd01065f11a77880887f37142c6cdea451ad
anseong1 <g77467944@gmail.com>
2026-03-12 00:19:20 +0900

    Add star pattern printing program
    Implement a program to print a star pattern.

--- ch02-1/실습과제4/star.cpp
+++ ch02-1/실습과제4/star.cpp
@@ -1,1 +1,17 @@
+// Title : star
+// Date : 2026.03.12
+// Author : 안성일
 
+#include <iostream>
+
+int main(void) {
+    for (int i = 0; i < 7; i++) {
+	   for (int j = 0; j < 7; j++) {
+			   if (i == j || i + j == 6) std::cout << "*";
+			   else std::cout << " ";
+		 }
+		 std::cout << std::endl;
+	}
+
+	  return 0;
+}
