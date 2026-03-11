9d60bef78277275cf8a6fb03919bea054054777f
anseong1 <g77467944@gmail.com>
2026-03-11 23:23:33 +0900

    Add info printing functionality in lnfo.cpp

--- ch02-1/실습과제2/lnfo.cpp
+++ ch02-1/실습과제2/lnfo.cpp
@@ -1,1 +1,14 @@
+﻿// Title : Print Info
+// Date : 2026.03.11
+// Author : 안성일
 
+#include<iostream>
+
+int main(void) {
+	  std::cout << "이름 : 홍길동" << std::endl;
+	  std::cout << "주소 : 군산시 대학로 558" << std::endl;
+	  std::cout << "학번 : 201012" << std::endl;
+	  std::cout << "차종 : 벤츠" << std::endl;
+
+	  return 0;
+}
