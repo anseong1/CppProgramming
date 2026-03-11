48d31fefc69ea7134cb5d04e44f8d57d071bfa86
anseong1 <g77467944@gmail.com>
2026-03-12 00:46:59 +0900

    Implement stair pattern printing in stair.cpp

--- ch02-1/실습과제5/stair.cpp
+++ ch02-1/실습과제5/stair.cpp
@@ -1,1 +1,14 @@
+// Title : Stair
+// Date : 2026.03.12
+// Author : 안성일
 
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	  for (int i = 0; i < 4; i++) {
+		    for (int j = 0; j <= i; j++) cout << "*";
+		    cout << endl;
+	  }
+}
