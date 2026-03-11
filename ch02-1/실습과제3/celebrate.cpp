84d2ad614f909cfbbc7d6e48dc06deadd3f80608
anseong1 <g77467944@gmail.com>
2026-03-11 23:51:52 +0900

    Add celebrate.cpp for graduation message output

--- ch02-1/실습과제3/celebrate.cpp
+++ ch02-1/실습과제3/celebrate.cpp
@@ -1,1 +1,13 @@
+// Title : celebrate
+// Date : 2026.03.11
+// Author : 안성일
 
+#include <iostream>
+
+int main(void) {
+	  std::cout << "군산대학교 \"홍길동\"" << std::endl;
+	  std::cout << "\"축하합니다.\"" << std::endl;
+	  std::cout << "100% \\취업율\\" << std::endl;
+
+	  return 0;
+}
