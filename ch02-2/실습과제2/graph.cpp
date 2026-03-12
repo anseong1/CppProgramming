174517475361eaa809cd4fe972fc7ce4cdfc9a47
anseong1 <g77467944@gmail.com>
2026-03-12 10:17:27 +0900

    Implement basic polynomial calculation in graph.cpp

--- ch02-2/실습과제2/graph.cpp
+++ ch02-2/실습과제2/graph.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int x;
+    cout << "정수 x를 입력 하시오: ";
+    cin >> x;
+    
+    int y = x*x + 2*x - 5;
+    
+    cout << "x=" << x << "일 때 y의 값은 " << y << "입니다." << endl;
+    
+    return 0;
+}
