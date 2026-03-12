ee0b1662867e312111b1468cc6aa2a9d2f8b48e7
anseong1 <g77467944@gmail.com>
2026-03-12 11:12:12 +0900

    Implement program to sum positive numbers from input

--- ch02-2/실습과제4/add.cpp
+++ ch02-2/실습과제4/add.cpp
@@ -1,1 +1,26 @@
+// Title : add
+// Date : 2026.03.12
+// Author : 안성일
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double nums[5];
+    double sum = 0;
+
+    cout << "실수 5개를 입력하세요>> ";
+    for (int i = 0; i < 5; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (nums[i] > 0) {
+            sum += nums[i];
+        }
+    }
+
+    cout << "양수 합은 " << sum << "입니다" << endl;
+
+    return 0;
+}
