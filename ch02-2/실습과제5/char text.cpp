5aa9ebd24e07172bcf5fff0dcee9b00058cad35b
anseong1 <g77467944@gmail.com>
2026-03-12 11:48:43 +0900

    Refactor char text program for clarity
    Fix formatting and improve code readability.

--- ch02-2/실습과제5/char text.cpp
+++ ch02-2/실습과제5/char text.cpp
@@ -1,1 +1,23 @@
+// TItie : char text
+// Date : 2026.03.12
+// Author : 안성일
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char text[100];
+
+    cout << "빈칸 없이 문자열을 입력하세요>> ";
+    cin >> text;
+
+    int len = strlen(text);
+
+    for (int i = 0; i < len; i++) {
+        cout << text[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
