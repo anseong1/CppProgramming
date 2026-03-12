dda838d1eb526bdbd08d85b5a62e1149735e4d45
anseong1 <g77467944@gmail.com>
2026-03-12 10:45:29 +0900

    Update information.cpp

--- ch02-2/실습과제3/information.cpp
+++ ch02-2/실습과제3/information.cpp
@@ -1,1 +1,30 @@
+// Title : information
+// Data : 2026.03.12
+// Author : 안성일
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int id;
+    string name, address;
+
+    cout << "학번을 입력하시오: ";
+    cin >> id;
+
+    cin.ignore();
+
+    cout << "이름을 입력하시오: ";
+    getline(cin, name);
+
+    cout << "주소를 입력하시오: ";
+    getline(cin, address);
+
+    cout << endl;
+    cout << "1. 학번:" << id << endl;
+    cout << "2. 이름:" << name << endl;
+    cout << "3. 주소:" << address << endl;
+
+    return 0;
+}
