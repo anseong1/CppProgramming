27979a477e571d5ad01be692f53bc18cb42ab13f
anseong1 <g77467944@gmail.com>
2026-03-16 17:11:37 +0900

    Update Triangle.cpp

--- ch03-1/실습과제2/Triangle.cpp
+++ ch03-1/실습과제2/Triangle.cpp
@@ -1,1 +1,24 @@
+// Title: Triangle
+// Date: 2026.03.16
+// Author: 안성일
 
+#include <iostream>
+using namespace std;
+
+class Triangle {
+public:
+	int height;
+	int width;
+	int getArea();
+
+};
+int Triangle::getArea() {
+	return height * width / 2;
+}
+int main() {
+	Triangle t;
+	t.height = 10;
+	t.width = 5;
+	cout << "삼각형의 면적은 " << t.getArea() << endl;
+	return 0;
+}
