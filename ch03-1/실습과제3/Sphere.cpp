ade85e79e2fe8eb4442a402aff4f8c35ba7787d4
anseong1 <g77467944@gmail.com>
2026-03-17 14:09:40 +0900

    Update Sphere.cpp

--- ch03-1/실습과제3/Sphere.cpp
+++ ch03-1/실습과제3/Sphere.cpp
@@ -1,1 +1,30 @@
+// Titele: Shere
+// Date: 2026.03.17
+// Author: 안성일
 
+#include <iostream>
+using namespace std;
+
+class Sphere {
+	public:
+	double radius;
+
+    double getVolume() {
+        return (4.0 / 3.0) * 3.14 * radius * radius * radius;
+    }
+
+    double getSurfaceArea() {
+        return (4 * 3.14 * radius * radius);
+    }
+};
+int main(){
+    
+    Sphere s;
+    s.radius = 5;
+
+    cout << "구의 부피는: " << s.getVolume() << endl;
+    cout << "구의 표면적은: " << s.getSurfaceArea() << endl;
+
+    return 0;
+
+}
