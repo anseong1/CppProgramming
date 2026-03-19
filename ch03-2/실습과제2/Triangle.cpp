19d94fc4a8167eef32de447cf0927ef4e53ceb9c
anseong1 <g77467944@gmail.com>
2026-03-19 09:52:17 +0900

    Update Triangle.cpp

--- ch03-2/실습과제2/Triangle.cpp
+++ ch03-2/실습과제2/Triangle.cpp
@@ -1,1 +1,35 @@
+// Titele: Triangle
+// Date: 2026.03.19
+// Author: 안성일
 
+#include <iostream>
+
+using namespace std;
+
+class Triangle {
+private:
+    double base;
+    double height;
+    double area;
+
+public:
+    Triangle(double b, double h) : base(b), height(h), area(0.5 * b * h) {
+    }
+
+    Triangle() : Triangle(1.0, 1.0) {
+    }
+
+    double getBase() { return base; }
+    double getHeight() { return height; }
+    double getArea() { return area; }
+};
+
+int main() {
+    Triangle tri1;
+    cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+
+    Triangle tri2(2, 4);
+    cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+
+    return 0;
+}
