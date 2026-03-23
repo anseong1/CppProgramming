d21711e8168facf0c1a021b6847e946395a81d77
anseong1 <g77467944@gmail.com>
2026-03-24 00:32:55 +0900

    Create SphereGlobalObject.cpp

---
+++ ch03-3/실습과제5/SphereGlobalObject.cpp
@@ -0,0 +1,39 @@
+// Title: SphereGlovalObject.cpp
+// Date: 2026.03.24
+// Author: 안성일
+
+#include <iostream>
+using namespace std;
+
+class Sphere {
+    double radius;
+public:
+    Sphere();
+    Sphere(double r);
+    ~Sphere();
+    double getVolume();
+};
+
+Sphere::Sphere() : Sphere(1) {}
+
+Sphere::Sphere(double r) : radius(r) {
+    cout << "반지름 " << radius << "인 구 생성" << endl;
+}
+
+Sphere::~Sphere() {
+    cout << "반지름 " << radius << "인 구 소멸" << endl;
+}
+
+double Sphere::getVolume() {
+    return (4.0 / 3.0) * 3.14 * radius * radius * radius;
+}
+
+// 전역 객체 선언
+Sphere sph1(10);
+Sphere sph2(20);
+
+int main() {
+    cout << "구의 부피는" << sph1.getVolume() << endl;
+    cout << "구의 부피는" << sph2.getVolume() << endl;
+    return 0;
+}
