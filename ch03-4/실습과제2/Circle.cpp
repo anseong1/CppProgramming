f2b5ed6dc03d35faa27d09273ba281d1d8093729
anseong1 <g77467944@gmail.com>
2026-03-25 10:39:53 +0900

    Update Circle.cpp

--- ch03-4/실습과제2/Circle.cpp
+++ ch03-4/실습과제2/Circle.cpp
@@ -1,1 +1,28 @@
+// Title: Circle
+// Date: 2026.03.25
+// Author: 안성일
 
+#include <iostream>
+using namespace std;
+
+class Circle {
+private:
+    int radius;         
+public:
+    Circle();
+    void setRadius(int r);   
+    int getRadius();         
+};
+
+Circle::Circle() { radius = 1; }
+
+void Circle::setRadius(int r) { radius = r; }
+
+int Circle::getRadius() { return radius; }
+
+int main() {
+    Circle moon;
+    moon.setRadius(5); 
+    cout << "원의 반지름은 " << moon.getRadius() << endl;
+    return 0;
+}
