a5a57f976208644e454a7b14702d1cd2b684b433
anseong1 <g77467944@gmail.com>
2026-03-31 21:56:58 +0900

    Update Picture.cpp

--- ch03-5/실습문제3/Picture.cpp
+++ ch03-5/실습문제3/Picture.cpp
@@ -3,47 +3,66 @@
 // Author: 안성일
 
 #include <iostream>
-#include <cstring> 
 using namespace std;
 
 class Picture {
 private:
     int width;
     int height;
-    char place[20]; 
+    char place[20];
 
 public:
-    Picture() {
-        width = 5;
-        height = 7;
-        strcpy(place, "모름");
-    }
+    Picture();
+    Picture(int w, int h, const char* p);
 
-    
-    Picture(int w, int h, const char* p) {
-        width = w;
-        height = h;
-        strcpy(place, p);
-    }
-
-  
-    int getWidth() {
-        return width;
-    }
-
-    int getHeight() {
-        return height;
-    }
-
-    const char* getPlace() {
-        return place;
-    }
+    int getWidth();
+    int getHeight();
+    const char* getPlace();
 };
+
+
+Picture::Picture() {
+    width = 5;
+    height = 7;
+
+    place[0] = 'N';
+    place[1] = 'o';
+    place[2] = 'n';
+    place[3] = 'e';
+    place[4] = '\0';  
+}
+
+
+Picture::Picture(int w, int h, const char* p) {
+    width = w;
+    height = h;
+
+    int i = 0;
+    while (p[i] != '\0') {
+        place[i] = p[i];
+        i++;
+    }
+    place[i] = '\0';  
+}
+
+int Picture::getWidth() {
+    return width;
+}
+
+int Picture::getHeight() {
+    return height;
+}
+
+const char* Picture::getPlace() {
+    return place;
+}
 
 int main() {
     Picture pic;
-    Picture mt(10, 14, "한라산");
+    Picture mt(10, 14, "Halla");
 
     cout << pic.getWidth() << "x" << pic.getHeight() << " " << pic.getPlace() << endl;
     cout << mt.getWidth() << "x" << mt.getHeight() << " " << mt.getPlace() << endl;
+
+    return 0;
 }
