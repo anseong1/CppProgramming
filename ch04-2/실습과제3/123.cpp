aa4d4ab5cd088ddbbace004ba5c4dee1c162d8d0
anseong1 <g77467944@gmail.com>
2026-04-06 10:58:15 +0900

    Update 123.cpp

--- ch04-2/실습과제3/123.cpp
+++ ch04-2/실습과제3/123.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     int n;
 
-    cout << "입력할 정수의 개수는? ";
+    cout << "입력할 정수의 개수를 입력하세요: ";
     cin >> n;
     cout << n << "개의 실수를 입력하시오." << endl;
 
@@ -23,7 +23,7 @@ int main() {
     for (int i = 0; i < n; i++)
         sum += p[i];
 
-    cout << "평균 =  " << sum / n << endl;
+    cout << "평균값은 " << sum / n << "입니다." << endl;
 
     delete[] p;
     return 0;
