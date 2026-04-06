ad5b25d73d9d93542db94eeeb9c73b76be3aa075
anseong1 <g77467944@gmail.com>
2026-04-06 10:52:48 +0900

    Create 123.cpp

---
+++ ch04-2/실습과제3/123.cpp
@@ -0,0 +1,30 @@
+// Title: 
+// Date: 2026.04.06
+// Author: 안성일
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "입력할 정수의 개수는? ";
+    cin >> n;
+    cout << n << "개의 실수를 입력하시오." << endl;
+
+    int* p = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << "번째 정수: ";
+        cin >> p[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += p[i];
+
+    cout << "평균 =  " << sum / n << endl;
+
+    delete[] p;
+    return 0;
+}
