3b651a2abce050c3e94300decb4a1a3c1f736c92
anseong1 <g77467944@gmail.com>
2026-04-14 14:34:08 +0900

    CharacterCounter

---
+++ ch04-4/실습문제3/CharacterCounter.cpp
@@ -0,0 +1,26 @@
+// Title: CharacterCounter
+// Date: 2026.04.14
+// Author: 안성일
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "문자열 입력 : ";
+    getline(cin, str);
+
+    int count = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'a') {
+            count++;
+        }
+    }
+
+    cout << "문자 a는 " << count << "개 있습니다." << endl;
+
+    return 0;
+}
