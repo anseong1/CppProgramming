fa2c1cec737eea0a5ef9776537b0b417b157bda6
anseong1 <g77467944@gmail.com>
2026-05-06 16:58:09 +0900

    add S

--- ch05-1/실습과제4/SwapString.cpp
+++ ch05-1/실습과제4/SwapString.cpp
@@ -1,3 +1,23 @@
 // Title: SwapString
 // Date: 2026.05.06
 // Author: 안성일
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void SwapString(string* a, string* b);
+
+int main() {
+    string s1("hello");
+    string s2("world");
+    cout << "호출전 s1:" << s1 << " s2:" << s2 << endl;
+    SwapString(&s1, &s2);  
+    cout << "호출후 s1:" << s1 << " s2:" << s2 << endl;
+}
+
+void SwapString(string* a, string* b) {
+    string temp = *a;
+    *a = *b;
+    *b = temp;
+}
