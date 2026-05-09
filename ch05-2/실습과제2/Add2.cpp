e4c4ecb2b4682f84818ac56ba5f2cd66dd119aaf
anseong1 <g77467944@gmail.com>
2026-05-10 01:11:23 +0900

    Add Add2.cpp

---
+++ ch05-2/실습과제2/Add2.cpp
@@ -0,0 +1,21 @@
+// Title: Add2
+// Date: 2026.05.10
+// Author: 안성일
+
+#include<iostream>
+using namespace std;
+
+void add2(int &value);
+int main(void)
+{
+    int number;
+    cout << "정수를 입력하세요 : ";
+    cin >> number;
+    add2(number);
+    cout << "2만큼 증가한 값 :" << number << endl;
+    return 0;
+}
+void add2(int &value)
+{
+    value += 2;
+}
